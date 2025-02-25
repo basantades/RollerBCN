e1d553dbe7c48297e725deb9a6dd48b61d632601
Alberto Basanta <dasantades@gmail.com>
2025-02-25 08:20:51 +0100

    menu routeLinks

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { MapaComponent } from './components/mapa/mapa.component';
+import { CalendarioComponent } from './components/calendario/calendario.component';
+import { GraficosComponent } from './components/graficos/graficos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'mapa', component: MapaComponent }, 
+    { path: 'calendario', component: CalendarioComponent},
+    { path: 'graficos', component: GraficosComponent },
+    { path: '**', redirectTo: '' } 
+];
