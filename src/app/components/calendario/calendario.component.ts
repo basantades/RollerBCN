5460784a2fbcc72a579c920307a714363a9027d6
Alberto Basanta <dasantades@gmail.com>
2025-02-26 23:35:43 +0100

    calendar install ok

--- src/app/components/calendario/calendario.component.ts
+++ src/app/components/calendario/calendario.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import { CalendarOptions } from '@fullcalendar/core'; // useful for typechecking
+import dayGridPlugin from '@fullcalendar/daygrid';
 
 @Component({
   selector: 'app-calendario',
-  imports: [],
+  imports: [FullCalendarModule],
   templateUrl: './calendario.component.html',
   styleUrl: './calendario.component.scss'
 })
 export class CalendarioComponent {
-
+  calendarOptions: CalendarOptions = {
+    initialView: 'dayGridMonth',
+    plugins: [dayGridPlugin]
+  };
 }
